e1c102053dcc4fde40f4c247398f5b330c31dadd
zarwaan <zarwaanshroff@gmail.com>
2024-02-08 02:13:14 +0530

    can mark tickets

--- TicketGenerator/ticket.js
+++ TicketGenerator/ticket.js
@@ -55,41 +55,66 @@ function generateTicketNumbers(thisRow,i)
         return generateTicketNumbers(thisRow,i);
     return number;
 }
-console.log(numbersGenerated[0][0]);
-console.log('hi');
+
 document.querySelector(".ticket").innerHTML = `
 <tr>
-    <td>${numbersGenerated[0][0]}</td>
-    <td>${numbersGenerated[1][0]}</td>
-    <td>${numbersGenerated[2][0]}</td>
-    <td>${numbersGenerated[3][0]}</td>
-    <td>${numbersGenerated[4][0]}</td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[0][0]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[2][0]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[3][0]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[4][0]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[1][0]}</div></td>
 </tr>
 <tr>
-    <td>${numbersGenerated[0][1]}</td>
-    <td>${numbersGenerated[1][1]}</td>
-    <td>${numbersGenerated[2][1]}</td>
-    <td>${numbersGenerated[3][1]}</td>
-    <td>${numbersGenerated[4][1]}</td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[0][1]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[1][1]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[2][1]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[3][1]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[4][1]}</div></td>
 </tr>
 <tr>
-    <td>${numbersGenerated[0][2]}</td>
-    <td>${numbersGenerated[1][2]}</td>
-    <td><img src="tubelight.jpg" id="tubelight"></td>
-    <td>${numbersGenerated[3][2]}</td>
-    <td>${numbersGenerated[4][2]}</td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[0][2]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[1][2]}</div></td>
+    <td id="middle-cell"><img src="tubelight.jpg" id="tubelight"></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[3][2]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[4][2]}</div></td>
 </tr>
 <tr>
-    <td>${numbersGenerated[0][3]}</td>
-    <td>${numbersGenerated[1][3]}</td>
-    <td>${numbersGenerated[2][3]}</td>
-    <td>${numbersGenerated[3][3]}</td>
-    <td>${numbersGenerated[4][3]}</td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[0][3]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[1][3]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[2][3]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[3][3]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[4][3]}</div></td>
 </tr>
 <tr>
-    <td>${numbersGenerated[0][4]}</td>
-    <td>${numbersGenerated[1][4]}</td>
-    <td>${numbersGenerated[2][4]}</td>
-    <td>${numbersGenerated[3][4]}</td>
-    <td>${numbersGenerated[4][4]}</td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[0][4]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[1][4]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[2][4]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[3][4]}</div></td>
+    <td><img src="circle.png" id="circle"><div>${numbersGenerated[4][4]}</div></td>
 </tr>`;
+
+document.querySelectorAll("td img").forEach(
+    function(circle) 
+    {
+        if(circle.id!="tubelight")
+            circle.style.visibility = "hidden";
+    }
+);
+
+
+var cells = Array.prototype.slice.call(document.getElementsByTagName("td"), 0);
+console.log(cells.length);
+for(let i=0; i<cells.length; i++)
+{
+    if(cells[i].id != "middle-cell")
+    {
+        cells[i].addEventListener('click',mark);
+    }
+}
+function mark()
+{
+    if(this.firstChild.style.visibility == "visible")
+        this.firstChild.style.visibility = "hidden";
+    else
+        this.firstChild.style.visibility = "visible";
+}
